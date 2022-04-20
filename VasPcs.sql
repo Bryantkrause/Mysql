7654af82879fb643bbce280d3411a639e390dc84
bryantkrause <bryantkrause@gmail.com>
2022-04-20 16:30:02 -0700

    Rates

--- VasPcs.sql
+++ VasPcs.sql
@@ -1,11 +1,25 @@
 SELECT
-SO.OrderNumber, ShipmentOrderPick.WarehouseSku, SO.User2, PackDetail.UOM, SO.ActualShipDate, SO.CustomerName, PackDetail.Count, ShipmentOrderPick.Qty
-FROM ShipmentOrder As SO INNER JOIN ShipmentOrderPick 
-ON SO.OrderNumber=ShipmentOrderPick.OrderNumber
+ShipShipmentOrder.OrderNumber, ShipmentOrderPick.WarehouseSku,ShipmentOrder.User2, PackDetail.UOM,
+ShipShipmentOrder.ActualShipDate,ShipmentOrder.CustomerName, PackDetail.Count, ShipmentOrderPick.Qty
+FROM ShipmentOrder AsShipmentOrder INNER JOIN ShipmentOrderPick 
+ONShipmentOrder.OrderNumber=ShipmentOrderPick.OrderNumber
 INNER JOIN Sku ON ShipmentOrderPick.WarehouseSku=Sku.WarehouseSku
 INNER JOIN PackDetail ON Sku.DefaultPack=PackDetail.PackName
-WHERE SO.CustomerName='400' AND SO.ActualShipDate BETWEEN '11/01/2021 00:00:01' AND '11/30/2021 23:59:59'
-AND (SO.User2='true' OR SO.User2='TRUE')
+WHERE ShipmentOrder.CustomerName='400' ANDShipmentOrder.ActualShipDate BETWEEN '11/01/2021 00:00:01' AND '11/30/2021 23:59:59'
+AND (ShipShipmentOrder.User2='true' ORShipmentOrder.User2='TRUE')
 AND PackDetail.UOM='IN'
 
-47895
+'M'
+SELECT
+ShipmentOrder.OrderNumber, SUM(ShipmentOrderPick.Qty), (SUM(ShipmentOrderPick.Qty) * CustomerTariff.Rate)
+FROM ShipmentOrder  INNER JOIN ShipmentOrderPick 
+ON ShipmentOrder.OrderNumber=ShipmentOrderPick.OrderNumber
+INNER JOIN Sku ON ShipmentOrderPick.WarehouseSku=Sku.WarehouseSku
+INNER JOIN PackDetail ON Sku.DefaultPack=PackDetail.PackName
+LEFT JOIN CustomerTariff ON ShipmentOrder.CustomerName=CustomerTariff.CustomerName 
+WHERE ShipmentOrder.CustomerName='400'
+"&Date&" 
+AND (ShipmentOrder.User2='true' OR ShipmentOrder.User2='TRUE')
+AND PackDetail.UOM='IN'
+AND CustomerTariff.TariffName = 'VAS PCS'
+GROUP BY ShipmentOrder.OrderNumber, CustomerTariff.Rate
