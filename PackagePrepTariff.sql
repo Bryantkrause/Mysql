a30ca5ebb3696f07ed94b05178f70f46ec6b728f
bryantkrause <bryantkrause@gmail.com>
2022-04-20 14:36:03 -0700

    adding rates

--- PackagePrepTariff.sql
+++ PackagePrepTariff.sql
@@ -1,9 +1,28 @@
 SELECT ShipmentOrder.OrderNumber,COUNT(ShipmentOrder.CartonQuantity)
     From ShipmentOrder
     INNER JOIN ShipmentOrderPick ON ShipmentOrderPick.OrderNumber=ShipmentOrder.OrderNumber
+    LEFT JOIN LEFT JOIN CustomerTariff ON ShipmentOrder.CustomerName=CustomerTariff.CustomerName 
     WHERE ShipmentOrder.FacilityName='Cerritos' AND ShipmentOrder.CustomerName='400' AND ShipmentOrder.TransportMethod='U'
-    AND ShipmentOrder.ActualShipDate BETWEEN '11/01/2021 00:00:01' AND '11/30/2021 23:59:59' 
+    AND ShipmentOrder.ActualShipDate BETWEEN '11/01/2021 00:00:01' AND '11/30/2021 23:59:59'
     AND ShipmentOrderPick.WarehouseSku IN(
     SELECT Sku.WarehouseSku  From Sku INNER JOIN PackDetail ON Sku.DefaultPAck=PackDetail.PackName 
     Where Sku.CustomerName='400' AND PackDetail.UOM='IN' AND Sku.User12='PACKAGE PREP CASE')
     GROUP BY ShipmentOrder.OrderNumber
+
+'update with M'
+SELECT ShipmentOrder.OrderNumber, COUNT(ShipmentOrder.CartonQuantity) AS QTY, (COUNT(ShipmentOrder.CartonQuantity) * CustomerTariff.Rate) AS Charge
+From ShipmentOrder
+INNER JOIN ShipmentOrderPick ON ShipmentOrderPick.OrderNumber=ShipmentOrder.OrderNumber
+LEFT JOIN CustomerTariff ON ShipmentOrder.CustomerName=CustomerTariff.CustomerName 
+WHERE ShipmentOrder.FacilityName='Cerritos'
+AND ShipmentOrder.CustomerName='400'
+AND ShipmentOrder.TransportMethod='U'
+"&Date&" 
+AND CustomerTariff.TariffName = 'PACKAGE PREP CS'
+AND ShipmentOrderPick.WarehouseSku
+IN(
+SELECT Sku.WarehouseSku  
+From Sku 
+INNER JOIN PackDetail ON Sku.DefaultPAck=PackDetail.PackName 
+Where Sku.CustomerName='400' AND PackDetail.UOM='IN' AND Sku.User12='PACKAGE PREP CASE')
+GROUP BY ShipmentOrder.OrderNumber, ShipmentOrder.CartonQuantity, CustomerTariff.Rate
--- Picks.sql
+++ Picks.sql
@@ -14,24 +14,25 @@
     LEFT JOIN CustomerTariff ON ShipmentOrder.CustomerName=CustomerTariff.CustomerName 
     WHERE ShipmentOrder.FacilityName='Cerritos' AND ShipmentOrder.CustomerName='400' AND ShipmentOrderPick.PickUOM!='PL'"&Date&" AND
     ShipmentOrderPick.PickUOM = 'CS' AND CustomerTariff.TariffName = 'HNDL OUT CS/UOM'
-    GROUP BY ShipmentOrderPick.OrderNumber, ShipmentOrderPick.PickUOM, ShipmentOrderPick.PickQty, CustomerTariff.Rate ORDER BY OrderNumber
+    GROUP BY ShipmentOrderPick.OrderNumber, ShipmentOrderPick.PickUOM, CustomerTariff.Rate ORDER BY OrderNumber
 
     'M'
    'IN'
    'something is broken with this order 0000158311 is duplicating charges not combining'
+   'k figured it out pick qty was different and creating separate rows'
    SELECT 
-    ShipmentOrderPick.OrderNumber, ShipmentOrderPick.PickUOM, SUM(ShipmentOrderPick.PickQty), (SUM(ShipmentOrderPick.PickQty) * CustomerTariff.Rate)
+    ShipmentOrderPick.OrderNumber, ShipmentOrderPick.PickUOM, (SUM(ShipmentOrderPick.PickQty) * CustomerTariff.Rate)
     FROM ShipmentOrder INNER JOIN  ShipmentOrderPick ON ShipmentOrder.OrderNumber=ShipmentOrderPick.OrderNumber
     LEFT JOIN CustomerTariff ON ShipmentOrder.CustomerName=CustomerTariff.CustomerName 
     WHERE ShipmentOrder.FacilityName='Cerritos' AND ShipmentOrder.CustomerName='400' AND ShipmentOrderPick.PickUOM!='PL'"&Date&" AND
     ShipmentOrderPick.PickUOM = 'IN' AND CustomerTariff.TariffName = 'HNDL OUT IN/UOM'
-    GROUP BY ShipmentOrderPick.OrderNumber, ShipmentOrderPick.PickUOM, ShipmentOrderPick.PickQty, CustomerTariff.Rate ORDER BY OrderNumber
+    GROUP BY ShipmentOrderPick.OrderNumber, ShipmentOrderPick.PickUOM, CustomerTariff.Rate ORDER BY OrderNumber
     'M'
    'EA'
    SELECT 
-    ShipmentOrderPick.OrderNumber, ShipmentOrderPick.PickUOM, SUM(ShipmentOrderPick.PickQty), (SUM(ShipmentOrderPick.PickQty) * CustomerTariff.Rate)
+    ShipmentOrderPick.OrderNumber, ShipmentOrderPick.PickUOM, (SUM(ShipmentOrderPick.PickQty) * CustomerTariff.Rate)
     FROM ShipmentOrder INNER JOIN  ShipmentOrderPick ON ShipmentOrder.OrderNumber=ShipmentOrderPick.OrderNumber
     LEFT JOIN CustomerTariff ON ShipmentOrder.CustomerName=CustomerTariff.CustomerName 
     WHERE ShipmentOrder.FacilityName='Cerritos' AND ShipmentOrder.CustomerName='400' AND ShipmentOrderPick.PickUOM!='PL'"&Date&" AND
     ShipmentOrderPick.PickUOM = 'EA' AND CustomerTariff.TariffName = 'HNDL OUT EA/UOM'
-    GROUP BY ShipmentOrderPick.OrderNumber, ShipmentOrderPick.PickUOM, ShipmentOrderPick.PickQty, CustomerTariff.Rate ORDER BY OrderNumber
+    GROUP BY ShipmentOrderPick.OrderNumber, ShipmentOrderPick.PickUOM, CustomerTariff.Rate ORDER BY OrderNumber
