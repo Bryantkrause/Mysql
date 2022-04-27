782839099b5e121756d18e9080f040fc9ea058e1
bryantkrause <bryantkrause@gmail.com>
2022-04-27 16:59:36 -0700

    better open orders included open work orders

--- DavinciOpenOrder.sql
+++ DavinciOpenOrder.sql
@@ -7,4 +7,12 @@ WHERE public.dv_order.doc_number NOT LIKE '%ADJ%' AND NOT (public.dv_order.statu
  (public.dv_order.actual_trans_ts >='2022-03-01 00:00:00' AND public.dv_order.actual_trans_ts< '2022-04-01 00:00:00')
  ORDER BY public.dv_order.type, public.dv_business_partner.description
 
-
+'work order'
+SELECT 
+public.dv_order.actual_trans_ts, public.dv_order.doc_number, public.dv_order.po_number, public.dv_order.type, public.dv_order.status, 
+dv_business_partner.description 
+FROM public.dv_order INNER JOIN public.dv_business_partner ON public.dv_order.vendor_id = public.dv_business_partner.id
+WHERE public.dv_order.doc_number NOT LIKE '%ADJ%' AND NOT (public.dv_order.status=6 OR public.dv_order.status=9 ) AND
+ (public.dv_order.Created_on_dt >='2022-04-01 00:00:00')
+ AND public.dv_order.type = 3
+ ORDER BY public.dv_order.type, public.dv_business_partner.description
---
+++ openWorkOrders.sql
@@ -0,0 +1,5 @@
+SELECT *
+
+FROM WorkOrder
+
+WHERE  DocumentDate > '4/1/2022 0:00:00 AM'
