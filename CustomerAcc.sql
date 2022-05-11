42a86f8b2647d8edad0d39b03fd8f27b240c4f64
bryantkrause <bryantkrause@gmail.com>
2022-05-11 16:24:50 -0700

    fabulous

--- CustomerAcc.sql
+++ CustomerAcc.sql
@@ -3,3 +3,10 @@ CustomerName, FacilityName, AccessorialName, Rate, MinimumCharge
 FROM CustomerAccessorial 
 WHERE CustomerName='400'
 
+'M'
+SELECT 
+    CustomerAccessorial.CustomerName,
+    CustomerAccessorial.FacilityName,
+    Accessorial.AccessorialAlias AS Charge,
+    CustomerAccessorial.Rate
+FROM CustomerAccessorial LEFT JOIN Accessorial ON CustomerAccessorial.AccessorialName=Accessorial.AccessorialName
