654d3886ca277eb35927dd02bc6428762393ad97
bryantkrause <bryantkrause@gmail.com>
2022-05-11 16:04:11 -0700

    modular type

--- CustomerTar.sql
+++ CustomerTar.sql
@@ -5,3 +5,12 @@ SELECT CustomerName,
     SplitMonthRate,
     MinimumCharge
 FROM CustomerTariff
+'M'
+SELECT 
+    CustomerTariff.CustomerName,
+    CustomerTariff.FacilityName,
+    Tariff.TariffAlias AS Charge,
+    CustomerTariff.Rate,
+    CustomerTariff.SplitMonthRate,
+    CustomerTariff.MinimumCharge
+FROM CustomerTariff LEFT JOIN Tariff ON CustomerTariff.TariffName=Tariff.TariffName
