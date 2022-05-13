66bfdd3eaae46ea2c3b58cb7e489fe93a5e81cfd
bryantkrause <bryantkrause@gmail.com>
2022-05-13 08:24:19 -0700

    IT WOOOOOOOORRRRKKKKKSSSSSSSSS

--- InvoiceActivity.sql
+++ InvoiceActivity.sql
@@ -16,9 +16,21 @@ WHERE DocumentInvoice.CustomerName = "&Customer&" AND "&Date2&" AND DocumentInvo
 '1 charge test query'
 'M'
 
-SELECT DocumentInvoice.OrderNumber,
+SELECT DocumentInvoice.OrderNumber
 SUM( CASE WHEN DocumentInvoiceDetail.ChargeAlias = 'Pallet Outbound' THEN DocumentInvoiceDetail.Amount ELSE 0 END) AS 'Pallet Outbound',
 SUM( CASE WHEN DocumentInvoiceDetail.ChargeAlias = 'Order Processing' THEN DocumentInvoiceDetail.Amount ELSE 0 END) AS 'Order Processing'
+FROM DocumentInvoice LEFT JOIN  DocumentInvoiceDetail ON DocumentInvoice.DocumentInvoiceNumber = DocumentInvoiceDetail.DocumentInvoiceNumber
+
+WHERE DocumentInvoice.CustomerName = "&Customer&" AND "&Date2&" AND DocumentInvoice.FacilityName!='Ztest'
+
+GROUP BY DocumentInvoice.OrderNumber
+
+
+"This is the working model need to create report to combine accesorial and tariff charges
+then clean upnaming schemas
+"
+SELECT DocumentInvoice.OrderNumber
+"&TestAcc&"
 FROM DocumentInvoice LEFT JOIN  DocumentInvoiceDetail ON DocumentInvoice.DocumentInvoiceNumber = DocumentInvoiceDetail.DocumentInvoiceNumber
 
 WHERE DocumentInvoice.CustomerName = "&Customer&" AND "&Date2&" AND DocumentInvoice.FacilityName!='Ztest'
