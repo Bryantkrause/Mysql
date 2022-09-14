85fc1d7d4723dab61dcd000cd67dae8064ab1481
bryantkrause <bryantkrause@gmail.com>
2022-09-13 17:30:10 -0700

    update inbound report

--- InboundPallets.sql
+++ InboundPallets.sql
@@ -10,17 +10,17 @@ GROUP BY CustomerName, DeliveryDate
 
 'Davinci inbound pallets get all inbound pallets based on date'
 
-SELECT dv_business_partner.description,EXTRACT(MONTH FROM dv_order.actual_trans_ts) as month, EXTRACT(YEAR FROM dv_order.actual_trans_ts) as YEAR,
- sum(dv_order_recv_item_entry.qty) as Units, count(DISTINCT(dv_order.id))
+SELECT dv_business_partner.description,
+EXTRACT(MONTH FROM dv_order.actual_trans_ts) as month,
+EXTRACT(YEAR FROM dv_order.actual_trans_ts) as YEAR,
+sum(dv_order_recv_item_entry.qty) as Units,
+count(DISTINCT(dv_order.id)) AS Orders,
+count(DISTINCT(dv_order_recv_item_entry.containerid)) AS Pallets
 FROM dv_order INNER JOIN dv_business_partner
-ON dv_order.vendor_id = dv_business_partner.id
-INNER JOIN dv_order_recv_item_entry ON dv_order.id = dv_order_recv_item_entry.order_id
-
-WHERE dv_order.actual_trans_ts BETWEEN '2021-05-01 00:00:00' AND '2022-07-31 23:59:59'
-
-GROUP BY dv_business_partner.description , month, YEAR
-ORDER BY dv_business_partner.description, month, YEAR
-
+ON dv_order.vendor_id = dv_business_partner.id INNER JOIN dv_order_recv_item_entry ON dv_order.id = dv_order_recv_item_entry.order_id
+WHERE dv_order.actual_trans_ts BETWEEN '2021-01-01 00:00:00' AND '2021-12-31 23:59:59' AND 
+dv_business_partner.accountid = '261'
+GROUP BY dv_business_partner.description , month, YEAR#(lf)ORDER BY dv_business_partner.description, month, YEAR
 
 'Ramp Inbound Pallets get all ramp customers total inbound pallets based on date'
 SELECT CustomerName, month(DeliveryDate) AS month,year(DeliveryDate) AS year, SUM(QtyReceived) As Units Count(DISTINCT(ReceiptNumber))
