a0a82eee47fd9e1a4e6854aa8320b8d55d063e99
bryantkrause <bryantkrause@gmail.com>
2022-09-23 16:15:51 -0700

    430 > 500

--- pivotar.sql
+++ pivotar.sql
@@ -13,7 +13,7 @@ ORDER BY year desc
 -- combine accounts with multiple accounts
 SELECT * FROM(
     SELECT  
-    CASE WHEN CustomerName >= 430 THEN 500 ELSE CustomerName END As ID,
+    CASE WHEN CustomerName >= 430 THEN 430 ELSE CustomerName END As ID,
 year(DeliveryDate) as year, IsNull(month(DeliveryDate), 0) as month ,
 PalletsReceived 
 FROM WarehouseReceipt
@@ -21,7 +21,7 @@ WHERE DeliveryDate BETWEEN '1/01/2021 00:00:01' AND '07/31/2022 23:59:59' AND
  CustomerName != 'PC' AND CustomerName !='Z_TEST')  test
  PIVOT (
     SUM(PalletsReceived) FOR month IN ([1] , [2] , [3] , [4] , [5] , [6] , [7] , [8] ,[9] , [10] , [11] , [12])) AS thingy
-ORDER BY year desc
+ORDER BY ID, year desc
 
 -- total
 SELECT CustomerName,
