412cd0444caa4c931b6fc5049c486bd513028c86
bryantkrause <bryantkrause@gmail.com>
2022-10-05 16:16:54 -0700

    update pallet rerport

--- Pallets.sql
+++ Pallets.sql
@@ -1,30 +1,29 @@
 SELECT
-ShipmentOrder.OrderNumber, ShipmentOrderPick.FromPalletId, Count( Distinct(ShipmentOrderPick.FromPalletId))
+ShipmentOrderPick.FromPalletId, Count( Distinct(ShipmentOrderPick.FromPalletId))
 FROM ShipmentOrder
 INNER JOIN ShipmentOrderPick ON
-ShipmentOrderPick.OrderNumber=ShipmentOrder.OrderNumber
-LEFT JOIN CustomerAccessorial ON
- ShipmentOrder.CustomerName=CustomerAccessorial.CustomerName 
+ShipmentOrderPick.OrderNumber=ShipmentOrder.OrderNumber AND
+ShipmentOrderPick.FacilityName=ShipmentOrder.FacilityName AND
+ShipmentOrderPick.CustomerName=ShipmentOrder.CustomerName 
 WHERE
 ShipmentOrder.FacilityName='Cerritos'
 AND ShipmentOrder.CustomerName='400'
-AND ShipmentOrder.TransportMethod ='M'
+AND ShipmentOrder.TransportMethod <>'U'
 AND ShipmentOrder.ActualShipDate BETWEEN '11/01/2021 00:00:01' AND '11/30/2021 23:59:59'
-GROUP BY ShipmentOrderPick.FromPalletId, ShipmentOrder.OrderNumber
+GROUP BY ShipmentOrderPick.FromPalletId
 
 "M"
 
 SELECT
-ShipmentOrder.OrderNumber, ShipmentOrderPick.FromPalletId, Count( Distinct(ShipmentOrderPick.FromPalletId)) As PalletNum, 
-(Count( Distinct(ShipmentOrderPick.FromPalletId)) * 14.50) As MaterialCharge, (Count( Distinct(ShipmentOrderPick.FromPalletId)) * 3.85) As WrapCharge
+ShipmentOrderPick.FromPalletId, Count( Distinct(ShipmentOrderPick.FromPalletId))
 FROM ShipmentOrder
 INNER JOIN ShipmentOrderPick ON
-ShipmentOrderPick.OrderNumber=ShipmentOrder.OrderNumber
-LEFT JOIN CustomerAccessorial ON
- ShipmentOrder.CustomerName=CustomerAccessorial.CustomerName 
+ShipmentOrderPick.OrderNumber=ShipmentOrder.OrderNumber AND
+ShipmentOrderPick.FacilityName=ShipmentOrder.FacilityName AND
+ShipmentOrderPick.CustomerName=ShipmentOrder.CustomerName 
 WHERE
 ShipmentOrder.FacilityName='Cerritos'
 AND ShipmentOrder.CustomerName='400'
-AND ShipmentOrder.TransportMethod ='M'
-"&Date&" 
-GROUP BY ShipmentOrderPick.FromPalletId, ShipmentOrder.OrderNumber
+AND ShipmentOrder.TransportMethod <>'U'
+AND "&Date4&"
+GROUP BY ShipmentOrderPick.FromPalletId
