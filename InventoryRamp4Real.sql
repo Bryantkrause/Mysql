a50d9a7135eda1d68346145180ecc753fe92f68e
bryantkrause <bryantkrause@gmail.com>
2022-10-07 16:08:20 -0700

    better

--- InventoryRamp4Real.sql
+++ InventoryRamp4Real.sql
@@ -11,6 +11,23 @@ GROUP BY CustomerName , Palletid, LotReferenceTwo
 SELECT CASE WHEN CustomerName >= 430 THEN 430 ELSE CustomerName END As ID,
 Palletid, WarehouseSku,ISNULL(TolocationName, 0) AS TolocationName,ISNULL(ToPalletId,0) AS ToPalletId, TransactionType, SUM(QTY)
 FROM InventoryTransaction
-WHERE CustomerName = '430' AND FacilityName = 'Fullerton'  
+WHERE  TransactionType = 4 AND TolocationName = 'Stage' OR TransactionType = 4 AND TolocationName = 'SHIPDOCK'
+CustomerName = '430' AND FacilityName = 'Fullerton'  
 GROUP BY CustomerName , Palletid, WarehouseSku,TransactionType, QTY,TolocationName,ToPalletId
 ORDER BY PalletId, TransactionType, WarehouseSku
+
+
+
+-- attempt
+WHERE
+    IF IsNumeric(@OrderNumber) = 1
+        OrderNumber = @OrderNumber
+    ELSE
+        OrderNumber LIKE '%' + @OrderNumber + '%'
+
+-- conversion
+WHERE 
+   (IsNumeric(@OrderNumber) AND (CAST OrderNumber AS VARCHAR) = (CAST @OrderNumber AS VARCHAR) OR
+   (NOT IsNumeric(@OrderNumber) AND OrderNumber LIKE ('%' + @OrderNumber))
+
+TolocationName = 'Stage' OR "SHIPDOCK"
