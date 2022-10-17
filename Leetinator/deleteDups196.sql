9414f0b55fd26a8f5b85e7c866bd6d0a5a77433f
bryantkrause <bryantkrause@gmail.com>
2022-10-17 11:00:39 -0700

    syntax is *

---
+++ Leetinator/deleteDups196.sql
@@ -0,0 +1,17 @@
+'Write an SQL query to delete all the duplicate emails, keeping only one unique email with the smallest id. Note that you are supposed to write a DELETE statement
+and not a SELECT one. After running your script, the answer shown is the Person table. The driver will first compile and 
+run your piece of code and then show the Person table. The final order of the Person table does not matter.
+The query result format is in the following example.'
+
+DELETE Person
+WHERE ID NOT IN
+(
+SELECT *
+FROM
+(SELECT min(id)
+FROM Person
+GROUP BY Email) as thing);
+
+DELETE FROM Person WHERE Id NOT IN 
+(SELECT * FROM(
+    SELECT MIN(Id) FROM Person GROUP BY Email) as p);
