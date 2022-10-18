0e6021ab45a0c244495066ab25122bc3a6e27624
bryantkrause <bryantkrause@gmail.com>
2022-10-18 16:39:03 -0700

    I hate you Davinci.

---
+++ csr/DavinciOrder.sql
@@ -0,0 +1,26 @@
+-- Inbound
+SELECT bp.accountid, 
+EXTRACT(YEAR FROM o.created_on_dt) as year,
+EXTRACT(Month FROM o.created_on_dt) as month,
+EXTRACT(Day FROM  o.created_on_dt) as day,
+count(o.doc_number) AS ORDERS,
+SUM(r.qty) AS QTY,
+COUNT(r.item_id) AS Sku
+FROM dv_business_partner AS bp INNER JOIN dv_order as o ON bp.id = o.vendor_id 
+INNER JOIN dv_order_recv_item_entry as r ON o.id = r.id
+WHERE o.created_on_dt BETWEEN '1/01/2022 00:00:01' AND '10/15/2022 23:59:59' AND o.type = 1
+GROUP BY bp.accountid, o.created_on_dt, year, month, day
+
+-- Outbound
+SELECT bp.accountid,
+EXTRACT(YEAR FROM o.created_on_dt) as Createyear,
+EXTRACT(Month FROM o.created_on_dt) as Createmonth,
+EXTRACT(Day FROM  o.created_on_dt) as Createday,
+count(o.doc_number) AS ORDERS,
+SUM(r.qty) AS QTY,
+COUNT(r.item_id) AS Sku
+FROM dv_business_partner AS bp INNER JOIN dv_order as o ON bp.id = o.vendor_id 
+INNER JOIN dv_order_ship_item_entry as r ON o.id = r.id
+WHERE o.created_on_dt BETWEEN '1/01/2022 00:00:01' AND '10/15/2022 23:59:59' AND o.type = 2 and o.actual_trans_ts IS NOT NULL
+GROUP BY bp.accountid, o.created_on_dt, Createyear, Createmonth, Createday
+
--- csr/RampOrders.sql
+++ csr/RampOrders.sql
@@ -1,6 +1,6 @@
 -- ramp out
 SELECT
-CASE WHEN ShipmentOrder.CustomerName >= 430 THEN 430 ELSE ShipmentOrder.CustomerName END As ID, 
+CASE WHEN ShipmentOrder.CustomerName >= 430 THEN 430 ELSE ShipmentOrder.CustomerName END As ID, ShipmentOrder.FacilityName,
 Year = DATEPART(YEAR, ShipmentOrder.CreateDate),
 Month = DATEPART(Month, ShipmentOrder.CreateDate),
 Day = DATEPART(Day, ShipmentOrder.CreateDate),
@@ -9,17 +9,17 @@ Count(DISTINCT(ShipmentOrderDetail.WarehouseSku)) As Sku
 FROM ShipmentOrder LEFT JOIN ShipmentOrderDetail ON ShipmentOrder.OrderNumber = ShipmentOrderDetail.OrderNumber
 WHERE ShipmentOrder.CreateDate  BETWEEN '1/01/2022 00:00:01' AND '10/15/2022 23:59:59' AND
 ShipmentOrder.CustomerName != 'PC' AND ShipmentOrder.CustomerName !='Z_TEST' AND ShipmentOrder.FacilityName !='Z_TEST'
-GROUP BY ShipmentOrder.CustomerName,DATEPART(Day, ShipmentOrder.CreateDate), DATEPART(YEAR, ShipmentOrder.CreateDate), DATEPART(Month, ShipmentOrder.CreateDate)
+GROUP BY ShipmentOrder.CustomerName,DATEPART(Day, ShipmentOrder.CreateDate), DATEPART(YEAR, ShipmentOrder.CreateDate), DATEPART(Month, ShipmentOrder.CreateDate), ShipmentOrder.FacilityName
 
 -- Ramp inbound
-SELECT CASE WHEN WarehouseReceipt.CustomerName >= 430 THEN 430 ELSE WarehouseReceipt.CustomerName END As name,
+SELECT CASE WHEN WarehouseReceipt.CustomerName >= 430 THEN 430 ELSE WarehouseReceipt.CustomerName END As name, WarehouseReceipt.FacilityName,
 Year = DATEPART(YEAR, WarehouseReceipt.CreateDate),
 Month = DATEPART(Month, WarehouseReceipt.CreateDate),
 Day = DATEPART(Day, WarehouseReceipt.CreateDate),
-SUM(WarehouseReceipt.QtyReceived) As Units, Count(DISTINCT(WarehouseReceipt.ReceiptNumber)) AS Orders,
+Count(DISTINCT(WarehouseReceipt.ReceiptNumber)) AS Orders, SUM(WarehouseReceipt.QtyReceived) As Qty,
 Count(WarehouseReceiptDetail.WarehouseSku) As Sku
 FROM WarehouseReceipt LEFT JOin WarehouseReceiptDetail ON WarehouseReceipt.ReceiptNumber = WarehouseReceiptDetail.ReceiptNumber
 WHERE WarehouseReceipt.CreateDate BETWEEN '1/01/2022 00:00:01' AND '10/15/2022 23:59:59'
 AND WarehouseReceipt.CustomerName != 'PC' AND WarehouseReceipt.CustomerName !='Z_TEST' AND WarehouseReceipt.FacilityName !='Z_TEST'
-GROUP BY WarehouseReceipt.CustomerName, DATEPART(Day, WarehouseReceipt.CreateDate), DATEPART(YEAR, WarehouseReceipt.CreateDate),DATEPART(Month, WarehouseReceipt.CreateDate)
+GROUP BY WarehouseReceipt.CustomerName, DATEPART(Day, WarehouseReceipt.CreateDate), DATEPART(YEAR, WarehouseReceipt.CreateDate),DATEPART(Month, WarehouseReceipt.CreateDate), WarehouseReceipt.FacilityName
 ORDER BY WarehouseReceipt.CustomerName, DATEPART(YEAR, WarehouseReceipt.CreateDate),DATEPART(Month, WarehouseReceipt.CreateDate)
