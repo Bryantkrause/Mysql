ce465069ace8340175756b70b64fa503d95e6d6c
bryantkrause <bryantkrause@gmail.com>
2022-11-07 15:05:27 -0800

    make note kind of less vague

--- ShipmentPickTypes.sql
+++ ShipmentPickTypes.sql
@@ -7,7 +7,7 @@ WHERE so.ActualShipDate BETWEEN '10/01/2022 00:00:01' AND '10/31/2022 23:59:59' 
 so.FacilityName = 'Fullerton'
 GROUP BY p.PickUOM
 
--- this query is a follow up of the above query and seletcs more detail to review at the order level not just the pick level
+-- this query is a follow up of the above query and seletcs more detail to review at the order level not just the pick level for a specific customer
 
 SELECT so.OrderNumber, p.PickUOM, SUM(p.PickQty) AS Total_Picks
 FROM ShipmentOrder as so LEFT JOIN ShipmentOrderPick as p On so.OrderNumber = p.OrderNumber AND 
@@ -16,7 +16,7 @@ WHERE so.ActualShipDate BETWEEN '10/01/2022 00:00:01' AND '10/31/2022 23:59:59' 
 so.FacilityName = 'Fullerton'
 GROUP BY so.OrderNumber, p.PickUOM
 
--- customer picks given type and time 
+-- all customer picks given type and time 
 
 SELECT so.CustomerName, p.PickUOM, SUM(p.PickQty) AS Total_Picks
 FROM ShipmentOrder as so LEFT JOIN ShipmentOrderPick as p On so.OrderNumber = p.OrderNumber AND 
@@ -24,7 +24,7 @@ so.CustomerName = p.CustomerName AND so.FacilityName = p.FacilityName
 WHERE so.ActualShipDate BETWEEN '10/01/2022 00:00:01' AND '10/31/2022 23:59:59' 
 GROUP BY so.CustomerName, p.PickUOM
 
--- customer picks given type and time buuuut with a pivot and combining Unicargo
+-- customer picks given type and time buuuut with a pivot and combining Unicargo also no null values
 
 with CTE as
 (
