00f9273ddf6d9747f91162e9e23a78de70d68f58
bryantkrause <bryantkrause@gmail.com>
2022-12-07 12:15:52 -0800

    days

--- Leetinator/Creation.sql
+++ Leetinator/Creation.sql
@@ -17,5 +17,24 @@ VALUES (
 ),
 (
     4, "Max", 90000 
-),
+)
 
+CREATE TABLE Weather(
+    id Int Primary Key,
+    recordDate date,
+    temperature int
+)
+
+INSERT INTO Weather
+VALUES (
+    1, 2015-01-01, 10
+),
+(
+    2, 2015-01-02, 25
+),
+(
+    3, 2015-01-03, 20 
+),
+(
+    4, 2015-01-04, 30 
+)
